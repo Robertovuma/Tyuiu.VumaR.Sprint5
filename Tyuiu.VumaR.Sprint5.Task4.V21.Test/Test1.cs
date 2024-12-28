47f56d3c9b4491909fe38af7a0c6fce290c6ce32
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 12:01:26 +0500

    Task4

---
+++ Tyuiu.VumaR.Sprint5.Task4.V21.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.VumaR.Sprint5.Task4.V21.Lib;
+namespace Tyuiu.VumaR.Sprint5.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint5.Task4.V21/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.VumaR.Sprint5.Task4.V21.Lib;
+namespace Tyuiu.VumaR.Sprint5.Task4.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
