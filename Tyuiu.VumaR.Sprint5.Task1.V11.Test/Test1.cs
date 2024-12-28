37f9454dce59812480a2c892707d0b9e7681609c
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 11:34:56 +0500

    Task1

---
+++ Tyuiu.VumaR.Sprint5.Task1.V11.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.VumaR.Sprint5.Task1.V11.Lib;
+namespace Tyuiu.VumaR.Sprint5.Task1.V11.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint5.Task1.V11/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.VumaR.Sprint5.Task1.V11.Lib;
+
+namespace Tyuiu.VumaR.Sprint5.Task1.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DataService ds = new();
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Начало: " + startValue);
+            Console.WriteLine("Конец: " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+            Console.ReadLine();
+        }
+    }
+}
