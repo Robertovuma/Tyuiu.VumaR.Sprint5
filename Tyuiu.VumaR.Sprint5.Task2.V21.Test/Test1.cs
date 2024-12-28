92fd4ef7acc056eb0cf68df391f569ab24b74925
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 11:40:15 +0500

    Task2

---
+++ Tyuiu.VumaR.Sprint5.Task2.V21.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.VumaR.Sprint5.Task2.V21.Lib;
+
+namespace Tyuiu.VumaR.Sprint5.Task2.V21.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint5.Task2.V21/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.VumaR.Sprint5.Task2.V21.Lib;
+
+namespace Tyuiu.VumaR.Sprint5.Task2.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[,] mtrx = new int[3, 3] { { 4, 8, 5 },
+                                          { 6, 3, 2 },
+                                          { 9, 4, 5 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
