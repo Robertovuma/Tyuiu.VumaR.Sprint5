50cc812ebae90103329229745dad237a8492bea7
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 11:27:12 +0500

    Task0

--- Tyuiu.VumaR.Sprint5.Task0.V5.Lib/DataService.cs
+++ Tyuiu.VumaR.Sprint5.Task0.V5.Lib/DataService.cs
@@ -5,11 +5,13 @@ namespace Tyuiu.VumaR.Sprint5.Task0.V5.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            File.WriteAllText(filePath, y.ToString("F3"));
             double z = Math.Log(((double)x + 1) / ((double)x + 2));
             z = Math.Round(z, 3);
-            File.WriteAllText(path, Convert.ToString(z));
-            return path;
+
+            return filePath;
+
         }
     }
 }
