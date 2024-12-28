6f11d4077694674df0e988107d63af96cedc4235
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 12:08:01 +0500

    Task5

---
+++ Tyuiu.VumaR.Sprint5.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.VumaR.Sprint5.Task5.V27.Lib
+{
+    public class DataService : ISprint5Task5V27
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (Convert.ToDouble(line) % 5 == 0)
+                    {
+                        res += Convert.ToDouble(line);
+                        count++;
+                    }
+                }
+            }
+            res /= count;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint5.Task5.V27/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.VumaR.Sprint5.Task5.V27.Lib;
+namespace Tyuiu.VumaR.Sprint5.Task5.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V27.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Среднее значение всех чисел кратных пяти = " + res);
+            Console.ReadKey();
+        }
+    }
+}
